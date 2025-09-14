784c756d35fb5cb5dcf87b468ab32aa7c899fc33
Christian Alexander Albarracin Adriano <christianalex-071@hotmail.com>
2025-09-14 12:21:50 -0500

    refactor version .net 9 to .net 6

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer("name=DefaultConnection"));
 
 builder.Services.AddScoped<IAttendeeService, AttendeeService>();
 builder.Services.AddScoped<IMeetingService, MeetingService>();
@@ -21,8 +21,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllers()
     .AddJsonOptions(options => { });
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
 
 // 1. Add CORS policy
 builder.Services.AddCors(options =>
@@ -39,11 +38,10 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsProduction())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.MapOpenApi();
 }
 
 // 2. Use CORS policy (before UseAuthorization and MapControllers)
