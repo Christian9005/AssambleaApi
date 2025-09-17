c6ff338f10b26d91d03e6170dce43e7e3bc9edf8
Christian Alexander Albarracin Adriano <christianalex-071@hotmail.com>
2025-09-17 14:26:56 -0500

    Update context

---
+++ Migrations/20250915034710_addedsecondvote.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssambleaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedsecondvote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SecondVote",
+                table: "Attendees",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SecondVote",
+                table: "Attendees");
+        }
+    }
+}
